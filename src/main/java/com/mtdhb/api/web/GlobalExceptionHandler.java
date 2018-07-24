5bf431492e8c3273fbebd1eaf0648ba7e3913510
小呆 <i@huangdenghe.com>
2018-07-24 22:20:07 +0800

    优化全局异常处理

--- src/main/java/com/mtdhb/api/web/GlobalExceptionHandler.java
+++ src/main/java/com/mtdhb/api/web/GlobalExceptionHandler.java
@@ -1,15 +1,13 @@
 package com.mtdhb.api.web;
 
-import java.util.stream.Collectors;
-
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.mtdhb.api.dto.Result;
 import com.mtdhb.api.exception.BusinessException;
 import com.mtdhb.api.util.Results;
 
@@ -19,42 +17,33 @@ import lombok.extern.slf4j.Slf4j;
  * @author i@huangdenghe.com
  * @date 2017/12/06
  */
-@RestControllerAdvice
+@ControllerAdvice
 @Slf4j
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(BusinessException.class)
-    public Result handleBusinessException(BusinessException e) {
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception e, Object body, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+        logger.warn(e.getMessage(), e);
+        return super.handleExceptionInternal(e, body, headers, status, request);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleBusinessException(BusinessException e) {
         log.warn(e.getSnapshot(), e);
-        return Results.error(e.getCode(), e.getMessage());
+        return new ResponseEntity<>(Results.error(e.getCode(), e.getMessage()), HttpStatus.OK);
     }
 
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public Result handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
-        log.warn(e.getMessage(), e);
-        return Results.error(HttpStatus.BAD_REQUEST.value(), "缺少参数", e.getParameterName());
-    }
-
-    @ExceptionHandler(BindException.class)
-    public Result handleBindException(BindException e) {
-        log.warn(e.getMessage(), e);
-        return Results.error(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                e.getFieldErrors().stream()
-                        .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage)));
-    }
-
-    @ExceptionHandler(RuntimeException.class)
-    public Result handleRuntimeException(RuntimeException e) {
+    @ExceptionHandler
+    public ResponseEntity<Object> handleRuntimeException(RuntimeException e) {
         log.error(e.getMessage(), e);
-        return Results.error(HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(Exception.class)
-    public Result exception(Exception e) {
+    @ExceptionHandler
+    public ResponseEntity<Object> handleException(Exception e) {
         log.error(e.getMessage(), e);
-        return Results.error(HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
