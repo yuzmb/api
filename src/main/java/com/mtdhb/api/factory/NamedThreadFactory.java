64b712c148bfae9d4d6a8080e358cf37cc5f097e
小呆 <i@huangdenghe.com>
2018-09-05 13:55:47 +0800

    Update Thread.UncaughtExceptionHandler

--- src/main/java/com/mtdhb/api/factory/NamedThreadFactory.java
+++ src/main/java/com/mtdhb/api/factory/NamedThreadFactory.java
@@ -13,10 +13,10 @@ import lombok.extern.slf4j.Slf4j;
 public class NamedThreadFactory implements ThreadFactory {
 
     private static final AtomicInteger POOL_NUMBER = new AtomicInteger(1);
+    private static final LogUncaughtExceptionHandler HANDLER = new LogUncaughtExceptionHandler();
     private final ThreadGroup GROUP;
     private final AtomicInteger THREAD_NUMBER = new AtomicInteger(1);
     private final String NAME_PREFIX;
-    private final LogUncaughtExceptionHandler HANDLER;
 
     public NamedThreadFactory(String name) {
         SecurityManager s = System.getSecurityManager();
@@ -25,19 +25,18 @@ public class NamedThreadFactory implements ThreadFactory {
             name = "pool";
         }
         NAME_PREFIX = name + "-" + POOL_NUMBER.getAndIncrement() + "-thread-";
-        HANDLER = new LogUncaughtExceptionHandler();
     }
 
     @Override
     public Thread newThread(Runnable r) {
         Thread t = new Thread(GROUP, r, NAME_PREFIX + THREAD_NUMBER.getAndIncrement(), 0);
+        t.setUncaughtExceptionHandler(HANDLER);
         if (t.isDaemon()) {
             t.setDaemon(false);
         }
         if (t.getPriority() != Thread.NORM_PRIORITY) {
             t.setPriority(Thread.NORM_PRIORITY);
         }
-        t.setUncaughtExceptionHandler(HANDLER);
         return t;
     }
 
